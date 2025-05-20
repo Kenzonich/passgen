b482044d119d4c9624d072013c4dc5e4f08e4dec
Kenzonich <kenzonicholson@gmail.com>
2025-05-20 10:46:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
+from logic import gen_pass
+import discord
+from discord.ext import commands
 import random
-hewan = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-password =""
-for i in range(10):
-    password +=random.choice ( hewan)
-print(password)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def password(ctx, length = 10):
+    await ctx.send(gen_pass(length))
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+bot.run("")
